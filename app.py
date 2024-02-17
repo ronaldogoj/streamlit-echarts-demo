d5f377bbad2217025fb802ea1b15f1cf6263bedc
Ronaldo Geraidine <ronaldogoj@gmail.com>
2024-02-16 21:34:18 -0300

    atualização do título

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from demo_pyecharts import ST_PY_DEMOS
 
 
 def main():
-    st.title("Streamlit ECharts Demo")
+    st.title("Streamlit ECharts Demo Ronaldo")
 
     with st.sidebar:
         st.header("Configuration")
