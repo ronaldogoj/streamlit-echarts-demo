6c2c7ff45dc3c029ff4c834aaedeb6ece2710604
Ronaldo Geraidine <ronaldogoj@gmail.com>
2024-02-17 00:09:04 -0300

    upload

--- app.py
+++ app.py
@@ -21,22 +21,7 @@ def main():
             options=api_options,
         )
 
-        '''
-        page_options = (
-            list(ST_PY_DEMOS.keys())
-            if selected_api == "pyecharts"
-            else list(ST_DEMOS.keys())
-        )
-        selected_page = st.selectbox(
-            label="Choose an example",
-            options=page_options,
-        )
-        demo, url = (
-            ST_DEMOS[selected_page]
-            if selected_api == "echarts"
-            else ST_PY_DEMOS[selected_page]
-        )
-        '''
+
         if selected_api == "echarts":
             st.caption(
                 """ECharts demos are extracted from https://echarts.apache.org/examples/en/index.html, 
