b39e69686ccdc62dc9bb3a09d4be233476f4f158
Ronaldo Geraidine <ronaldogoj@gmail.com>
2024-02-17 12:13:52 -0300

    upload

--- app.py
+++ app.py
@@ -55,13 +55,4 @@ if __name__ == "__main__":
         page_title="Streamlit ECharts Demo", page_icon=":chart_with_upwards_trend:"
     )
     main()
-    with st.sidebar:
-        st.markdown("---")
-        st.markdown(
-            '<h6>Made in &nbsp<img src="https://streamlit.io/images/brand/streamlit-mark-color.png" alt="Streamlit logo" height="16">&nbsp by <a href="https://twitter.com/andfanilo">@andfanilo</a></h6>',
-            unsafe_allow_html=True,
-        )
-        st.markdown(
-            '<div style="margin-top: 0.75em;"><a href="https://www.buymeacoffee.com/andfanilo" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="41" width="174"></a></div>',
-            unsafe_allow_html=True,
-        )
+
