580b58dc53e52f55ea14a7f69516ff42573376e3
Ronaldo Geraidine <ronaldogoj@gmail.com>
2024-02-16 22:39:32 -0300

    upload

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def main():
             return
 
         # Create tabs for displaying DataFrames
-        with st.tabs("DataFrame 1", "DataFrame 2"):
+        with st.tabs("DataFrame 1"):
             with st.tab("DataFrame 1"):
                 st.write(df)
 
